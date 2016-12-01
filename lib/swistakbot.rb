e2f2214fe5a0bafea3ff3744a0c2524cce729ca1
Rafał Łasocha <me@swistak35.com>
2016-12-01 01:01:31 +0100

    Add second example

--- lib/swistakbot.rb
+++ lib/swistakbot.rb
@@ -1,18 +1,29 @@
 require 'parser/current'
 
-class Swistakbot
+class ParseFileMethods
   Result = Struct.new(:klass, :method)
 
-  def initialize
+  def call(file)
+    ast = Parser::CurrentRuby.parse(file)
+    raise "Should be a class" if ast.type != :class
+    methods_block = ast.children.last
+    klass_name = ast.children.first.children[1].to_s
+    if methods_block.type == :begin
+      return methods_block.children.map {|m| parse_method(klass_name, m) }
+    elsif methods_block.type == :def
+      return [ parse_method(klass_name, methods_block) ]
+    else
+      raise "Not implemented yet"
+    end
   end
 
-  def parse_file_methods(string)
-    ast = Parser::CurrentRuby.parse(string)
-    raise "Should be a class" if ast.type != :class
-    klass_name = ast.children.first.children[1].to_s
-    method_name = ast.children.last.children.first.to_s
-    return [
-      Result.new(klass_name, method_name)
-    ]
+  private
+
+  def parse_method(klass_name, m)
+    method_name = m.children.first.to_s
+    Result.new(klass_name, method_name)
   end
+end
+
+class Swistakbot
 end
--- spec/swistakbot_spec.rb
+++ spec/swistakbot_spec.rb
@@ -1,22 +1,37 @@
 require 'spec_helper'
 
-
 RSpec.describe Swistakbot do
   specify do
     Swistakbot.new
   end
 
+  let(:parse_file_methods) { ParseFileMethods.new }
+
   specify do
-    swistakbot = Swistakbot.new
     file = <<END
       class Foo
         def bar
         end
       end
 END
-    r = swistakbot.parse_file_methods(file)
-    expect(r).to eq([
-      Swistakbot::Result.new("Foo", "bar")
+    expect(parse_file_methods.(file)).to eq([
+      ParseFileMethods::Result.new("Foo", "bar")
+    ])
+  end
+
+  specify do
+    file = <<END
+      class Foo
+        def bar
+        end
+
+        def baz
+        end
+      end
+END
+    expect(parse_file_methods.(file)).to eq([
+      ParseFileMethods::Result.new("Foo", "bar"),
+      ParseFileMethods::Result.new("Foo", "baz"),
     ])
   end
 end
