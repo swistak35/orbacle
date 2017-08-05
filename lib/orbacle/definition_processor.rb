06126ebd61ca8ebe990cce64668b40815a08e14d
Rafał Łasocha <me@swistak35.com>
2017-08-05 18:43:44 +0200

    up

--- lib/orbacle/definition_processor.rb
+++ lib/orbacle/definition_processor.rb
@@ -14,8 +14,30 @@ class Orbacle::DefinitionProcessor < Parser::AST::Processor
 
   def on_const(ast)
     name_loc_range = ast.loc.name
+    ast_all_ranges = all_ranges(ast)
+    require 'byebug'; byebug
     if @searched_line == name_loc_range.line && name_loc_range.column_range.include?(@searched_character)
       @found_constant = ast.children[1].to_s
+    end
+  end
+
+  def all_ranges(ast)
+    if ast.nil?
+      []
+    elsif ast.type == :cbase
+      []
+    else
+      all_ranges(ast.children[0]) + [ast.loc.name.column_range]
+    end
+  end
+
+  def const_to_string(ast)
+    if ast.nil?
+      nil
+    elsif ast.type == :cbase
+      ""
+    else
+      [const_to_string(ast.children[0]), ast.children[1]].compact.join("::")
     end
   end
 
--- spec/definition_processor_spec.rb
+++ spec/definition_processor_spec.rb
@@ -16,6 +16,21 @@ END
     expect(definition_processor(file, 3, 13)).to be_nil
   end
 
+  specify do
+    file = <<END
+      class Foo
+        def bar
+          ::Bar::Baz.new
+        end
+      end
+END
+    # expect(definition_processor(file, 3, 9)).to be_nil
+    expect(definition_processor(file, 3, 13)).to eq("::Bar::Baz")
+    # expect(definition_processor(file, 3, 11)).to eq("Baz")
+    # expect(definition_processor(file, 3, 12)).to eq("Baz")
+    # expect(definition_processor(file, 3, 13)).to be_nil
+  end
+
   def definition_processor(file, line, column)
     service = Orbacle::DefinitionProcessor.new
     service.process_file(file, line, column)
