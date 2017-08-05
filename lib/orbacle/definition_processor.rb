a29d9710afad7d83db3b6b9a367e9e30cb064bb3
Rafał Łasocha <me@swistak35.com>
2017-08-05 19:20:32 +0200

    Handle case of a class with a module

--- lib/orbacle/definition_processor.rb
+++ lib/orbacle/definition_processor.rb
@@ -14,46 +14,29 @@ class Orbacle::DefinitionProcessor < Parser::AST::Processor
 
   def on_const(ast)
     name_loc_range = ast.loc.name
-    if @searched_line == name_loc_range.line && name_loc_range.column_range.include?(@searched_character)
-      @found_constant = ast.children[1].to_s
+    ast_all_ranges = all_ranges(ast)
+    if @searched_line == name_loc_range.line && ast_all_ranges.any? {|r| r.include?(@searched_character) }
+      @found_constant = const_to_string(ast)
     end
   end
 
-  # def on_module(ast)
-  #   ast_name, _ = ast.children
-  #   prename, module_name = get_nesting(ast_name)
+  def all_ranges(ast)
+    if ast.nil?
+      []
+    elsif ast.type == :cbase
+      []
+    else
+      all_ranges(ast.children[0]) + [ast.loc.name.column_range]
+    end
+  end
 
-  #   @constants << [
-  #     scope_from_nesting_and_prename(@current_nesting, prename),
-  #     module_name,
-  #     :mod,
-  #     { line: ast_name.loc.line },
-  #   ]
-
-  #   current_nesting_element = [:mod, prename, module_name]
-  #   @current_nesting << current_nesting_element
-
-  #   super(ast)
-
-  #   @current_nesting.pop
-  # end
-
-  # def on_class(ast)
-  #   ast_name, _ = ast.children
-  #   prename, klass_name = get_nesting(ast_name)
-
-  #   @constants << [
-  #     scope_from_nesting_and_prename(@current_nesting, prename),
-  #     klass_name,
-  #     :klass,
-  #     { line: ast_name.loc.line },
-  #   ]
-
-  #   current_nesting_element = [:klass, prename, klass_name]
-  #   @current_nesting << current_nesting_element
-
-  #   super(ast)
-
-  #   @current_nesting.pop
-  # end
+  def const_to_string(ast)
+    if ast.nil?
+      nil
+    elsif ast.type == :cbase
+      ""
+    else
+      [const_to_string(ast.children[0]), ast.children[1]].compact.join("::")
+    end
+  end
 end
--- spec/definition_processor_spec.rb
+++ spec/definition_processor_spec.rb
@@ -16,9 +16,28 @@ END
     expect(definition_processor(file, 3, 13)).to be_nil
   end
 
+  specify do
+    file = <<END
+      class Foo
+        def bar
+          ::Bar::Baz.new
+        end
+      end
+END
+    expect(definition_processor(file, 3, 11)).to be_nil
+    expect(definition_processor(file, 3, 12)).to eq("::Bar::Baz")
+    expect(definition_processor(file, 3, 13)).to eq("::Bar::Baz")
+    expect(definition_processor(file, 3, 14)).to eq("::Bar::Baz")
+    expect(definition_processor(file, 3, 15)).to be_nil
+    expect(definition_processor(file, 3, 16)).to be_nil
+    expect(definition_processor(file, 3, 17)).to eq("::Bar::Baz")
+    expect(definition_processor(file, 3, 18)).to eq("::Bar::Baz")
+    expect(definition_processor(file, 3, 19)).to eq("::Bar::Baz")
+    expect(definition_processor(file, 3, 20)).to be_nil
+  end
+
   def definition_processor(file, line, column)
     service = Orbacle::DefinitionProcessor.new
     service.process_file(file, line, column)
   end
 end
-
