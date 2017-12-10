54167a97b4cb9f797cd09c3970ae0bca74df1a51
Rafał Łasocha <me@swistak35.com>
2017-12-10 15:06:25 +0100

    Added specs to CFG spec

--- spec/control_flow_graph_spec.rb
+++ spec/control_flow_graph_spec.rb
@@ -14,6 +14,18 @@ module Orbacle
         node(:int, { value: 42 }))
     end
 
+    specify do
+      snippet = <<-END
+      x = 42
+      END
+
+      r = generate_cfg(snippet)
+      expect(r).to include_edge(
+        node(:lvasgn, { var_name: "x" }),
+        node(:int, { value: 42 }))
+
+    end
+
     def generate_cfg(snippet)
       service = ControlFlowGraph.new
       service.process_file(snippet)
