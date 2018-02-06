2116a336cee7fe2886119d8ca72e6b2b1da5e61b
Rafał Łasocha <me@swistak35.com>
2018-02-06 12:21:15 +0100

    WIP

--- lib/orbacle/inference.rb
+++ lib/orbacle/inference.rb
@@ -3,7 +3,7 @@ module Orbacle
     TNominal = Struct.new(:name)
     TUnion = Struct.new(:elems)
     TVar = Struct.new(:index)
-    # TGeneric = Struct.new(:nominal, :type_vars)
+
     def call(equalities)
       assignments = {}
 
@@ -14,12 +14,47 @@ module Orbacle
 
         if left.is_a?(TVar)
           assignments[left.index] = right
-        # elsif right.is_a?(TVar)
-        #   assignments[right.index] = left
         end
       end
 
-      assignments
+      strict_assignments = assignments.dup
+      begin
+        varcount = count_variables(strict_assignments)
+        strict_assignments.each do |k, v|
+          strict_assignments[k] = concretize_type(v, strict_assignments)
+        end
+      end while count_variables(strict_assignments) < varcount
+
+      strict_assignments
+    end
+
+    def concretize_type(type, assignments)
+      case type
+      when TNominal
+        type
+      when TVar
+        assignments[type.index].dup
+      when TUnion
+        new_elems = type.elems.map {|e| concretize_type(e, assignments) }
+        TUnion.new(new_elems)
+      end
+    end
+
+    def count_variables(assgns)
+      assgns.values.map do |v|
+        count_variables_in_type(v)
+      end.sum
+    end
+
+    def count_variables_in_type(type)
+      case type
+      when TVar
+        1
+      when TNominal
+        0
+      when TUnion
+        type.elems.map(&method(:count_variables_in_type)).sum
+      end
     end
   end
 end
--- spec/inference_spec.rb
+++ spec/inference_spec.rb
@@ -5,7 +5,7 @@ module Orbacle
   RSpec.describe Inference do
     it do
       equalities = [
-        eq(tVar(0), tNominal("Integer"))
+        tEq(tVar(0), tNominal("Integer")),
       ]
 
       result = infer(equalities)
@@ -13,21 +13,54 @@ module Orbacle
       expect(result.size).to eq(1)
       expect(result[0]).to eq(tNominal("Integer"))
     end
-  end
 
-  def tVar(*args)
-    Inference::TVar.new(*args)
-  end
+    it do
+      equalities = [
+        tEq(tVar(0), tVar(1)),
+        tEq(tVar(1), tNominal("Integer")),
+      ]
 
-  def tNominal(*args)
-    Inference::TNominal.new(*args)
-  end
+      result = infer(equalities)
 
-  def infer(equalities)
-    Inference.new.(equalities)
-  end
+      expect(result.size).to eq(2)
+      expect(result[0]).to eq(tNominal("Integer"))
+      expect(result[1]).to eq(tNominal("Integer"))
+    end
 
-  def eq(left, right)
-    [left, right]
+    it do
+      equalities = [
+        tEq(tVar(0), tNominal("Integer")),
+        tEq(tVar(1), tNominal("String")),
+        tEq(tVar(2), tUnion([tVar(0), tVar(1)])),
+      ]
+
+      result = infer(equalities)
+
+      expect(result.size).to eq(3)
+      expect(result[0]).to eq(tNominal("Integer"))
+      expect(result[1]).to eq(tNominal("String"))
+      expect(result[2]).to eq(tUnion([tNominal("Integer"), tNominal("String")]))
+    end
+
+    def tVar(*args)
+      Inference::TVar.new(*args)
+    end
+
+    def tNominal(*args)
+      Inference::TNominal.new(*args)
+    end
+
+    def tUnion(*args)
+      raise if !args[0].is_a?(Array)
+      Inference::TUnion.new(*args)
+    end
+
+    def infer(equalities)
+      Inference.new.(equalities)
+    end
+
+    def tEq(left, right)
+      [left, right]
+    end
   end
 end
