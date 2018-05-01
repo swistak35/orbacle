82317dedf16b152f20617b855433f671e79d51f0
Rafał Łasocha <me@swistak35.com>
2018-05-01 12:38:20 +0200

    Add hardcoded Dir#glob

--- lib/orbacle/data_flow_graph/define_builtins.rb
+++ lib/orbacle/data_flow_graph/define_builtins.rb
@@ -8,6 +8,7 @@ module Orbacle
 
       def call
         add_object_klass
+        add_dir_klass
       end
 
       def add_object_klass
@@ -19,6 +20,17 @@ module Orbacle
             location: nil))
 
         define_object_opeq
+      end
+
+      def add_dir_klass
+        klass = @tree.add_klass(
+          GlobalTree::Klass.new(
+            name: "Dir",
+            scope: Scope.empty,
+            parent_ref: nil,
+            location: nil))
+
+        define_dir_glob
       end
 
       def define_object_opeq
@@ -33,6 +45,22 @@ module Orbacle
           nodes: GlobalTree::Method::Nodes.new(build_nodes_hash(arg_nodes), @graph.add_vertex(Node.new(:method_result)), [])))
         bool_node = Node.new(:bool)
         @graph.add_edge(bool_node, metod.nodes.result)
+      end
+
+      def define_dir_glob
+        arg_names = build_arg_names(1)
+        arg_nodes = build_arg_nodes(arg_names)
+        metod = @tree.add_method(GlobalTree::Method.new(
+          scope: Scope.new(["Dir"], false),
+          name: "glob",
+          location: nil,
+          args: build_arg_tree(arg_names),
+          visibility: :public,
+          nodes: GlobalTree::Method::Nodes.new(build_nodes_hash(arg_nodes), @graph.add_vertex(Node.new(:method_result)), [])))
+        str_node = Node.new(:str)
+        array_node = Node.new(:array)
+        @graph.add_edge(str_node, array_node)
+        @graph.add_edge(array_node, metod.nodes.result)
       end
 
       def build_arg_names(n)
