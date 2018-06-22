b49011dd82c6b41fe314b4339595c37a4fb40b15
Rafał Łasocha <me@swistak35.com>
2018-06-22 17:32:29 +0200

    Refactor operator assignments

--- lib/orbacle/data_flow_graph/builder/operator_assignment_processors.rb
+++ lib/orbacle/data_flow_graph/builder/operator_assignment_processors.rb
@@ -3,139 +3,76 @@ module Orbacle
     class Builder
       module OperatorAssignmentProcessors
         def handle_op_asgn(ast, context)
-          expr_partial_asgn = ast.children[0]
-          method_name = ast.children[1]
-          expr_argument = ast.children[2]
+          partial_assignment_ast, operator_name, argument_ast = ast.children
 
-          case expr_partial_asgn.type
-          when :lvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:send,
-                                                  [Parser::AST::Node.new(:lvar, [var_name]), method_name, expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :ivasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:send,
-                                                  [Parser::AST::Node.new(:ivar, [var_name]), method_name, expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :cvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:send,
-                                                  [Parser::AST::Node.new(:cvar, [var_name]), method_name, expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :casgn
-            scope = expr_partial_asgn.children[0]
-            var_name = expr_partial_asgn.children[1]
-            expr_full_rhs = Parser::AST::Node.new(:send,
-                                                  [Parser::AST::Node.new(:const, [scope, var_name]), method_name, expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :send
-            send_obj = expr_partial_asgn.children[0]
-            asgn_method_name = expr_partial_asgn.children[1]
-            args = expr_partial_asgn.children[2..-1]
-            expr_full_rhs = Parser::AST::Node.new(:send,
-                                                  [Parser::AST::Node.new(:send, [send_obj, asgn_method_name, *args]), method_name, expr_argument])
-            expr_full_asgn = expr_partial_asgn.updated(nil, [send_obj, "#{asgn_method_name}=", expr_full_rhs])
-          when :gvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:send,
-                                                  [Parser::AST::Node.new(:gvar, [var_name]), method_name, expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          else raise ArgumentError
-          end
-          expr_full_asgn_result = process(expr_full_asgn, context)
-
-          return expr_full_asgn_result
+          return process(
+            complete_assignment(
+              partial_assignment_ast,
+              Parser::AST::Node.new(:send, [
+                build_accessor_based_on_assignment(partial_assignment_ast),
+                operator_name,
+                argument_ast])),
+               context)
         end
 
         def handle_or_asgn(ast, context)
-          expr_partial_asgn = ast.children[0]
-          expr_argument = ast.children[1]
+          partial_assignment_ast, argument_ast = ast.children
 
-          case expr_partial_asgn.type
-          when :lvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:or,
-                                                  [Parser::AST::Node.new(:lvar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :ivasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:or,
-                                                  [Parser::AST::Node.new(:ivar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :cvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:or,
-                                                  [Parser::AST::Node.new(:cvar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :casgn
-            scope = expr_partial_asgn.children[0]
-            var_name = expr_partial_asgn.children[1]
-            expr_full_rhs = Parser::AST::Node.new(:or,
-                                                  [Parser::AST::Node.new(:const, [scope, var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          when :send
-            send_obj = expr_partial_asgn.children[0]
-            asgn_method_name = expr_partial_asgn.children[1]
-            args = expr_partial_asgn.children[2..-1]
-            expr_full_rhs = Parser::AST::Node.new(:or,
-                                                  [Parser::AST::Node.new(:send, [send_obj, asgn_method_name, *args]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.updated(nil, [send_obj, "#{asgn_method_name}=", expr_full_rhs])
-          when :gvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:or,
-                                                  [Parser::AST::Node.new(:gvar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
-          else raise ArgumentError
-          end
-          expr_full_asgn_result = process(expr_full_asgn, context)
-
-          return expr_full_asgn_result
+          return process(
+            complete_assignment(
+              partial_assignment_ast,
+              Parser::AST::Node.new(:or, [
+                build_accessor_based_on_assignment(partial_assignment_ast),
+                argument_ast])),
+            context)
         end
 
         def handle_and_asgn(ast, context)
-          expr_partial_asgn = ast.children[0]
-          expr_argument = ast.children[1]
+          partial_assignment_ast, argument_ast = ast.children
 
-          case expr_partial_asgn.type
+          return process(
+            complete_assignment(
+              partial_assignment_ast,
+              Parser::AST::Node.new(:and, [
+                build_accessor_based_on_assignment(partial_assignment_ast),
+                argument_ast])),
+               context)
+        end
+
+        def build_accessor_based_on_assignment(assignment_ast)
+          case assignment_ast.type
           when :lvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:and,
-                                                  [Parser::AST::Node.new(:lvar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
+            var_name = assignment_ast.children[0]
+            Parser::AST::Node.new(:lvar, [var_name])
           when :ivasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:and,
-                                                  [Parser::AST::Node.new(:ivar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
+            var_name = assignment_ast.children[0]
+            Parser::AST::Node.new(:ivar, [var_name])
           when :cvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:and,
-                                                  [Parser::AST::Node.new(:cvar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
+            var_name = assignment_ast.children[0]
+            Parser::AST::Node.new(:cvar, [var_name])
           when :casgn
-            scope = expr_partial_asgn.children[0]
-            var_name = expr_partial_asgn.children[1]
-            expr_full_rhs = Parser::AST::Node.new(:and,
-                                                  [Parser::AST::Node.new(:const, [scope, var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
+            scope = assignment_ast.children[0]
+            var_name = assignment_ast.children[1]
+            Parser::AST::Node.new(:const, [scope, var_name])
           when :send
-            send_obj = expr_partial_asgn.children[0]
-            asgn_method_name = expr_partial_asgn.children[1]
-            args = expr_partial_asgn.children[2..-1]
-            expr_full_rhs = Parser::AST::Node.new(:and,
-                                                  [Parser::AST::Node.new(:send, [send_obj, asgn_method_name, *args]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.updated(nil, [send_obj, "#{asgn_method_name}=", expr_full_rhs])
+            send_obj = assignment_ast.children[0]
+            asgn_method_name = assignment_ast.children[1]
+            args = assignment_ast.children[2..-1]
+            Parser::AST::Node.new(:send, [send_obj, asgn_method_name, *args])
           when :gvasgn
-            var_name = expr_partial_asgn.children[0]
-            expr_full_rhs = Parser::AST::Node.new(:and,
-                                                  [Parser::AST::Node.new(:gvar, [var_name]), expr_argument])
-            expr_full_asgn = expr_partial_asgn.append(expr_full_rhs)
+            var_name = assignment_ast.children[0]
+            Parser::AST::Node.new(:gvar, [var_name])
           else raise ArgumentError
           end
-          expr_full_asgn_result = process(expr_full_asgn, context)
+        end
 
-          return expr_full_asgn_result
+        def complete_assignment(partial_assignment_ast, full_rhs_ast)
+          if partial_assignment_ast.type == :send
+            send_obj_ast, accessor_method_name, _ = partial_assignment_ast.children
+            partial_assignment_ast.updated(nil, [send_obj_ast, "#{accessor_method_name}=", full_rhs_ast])
+          elsif [:lvasgn, :ivasgn, :cvasgn, :casgn, :gvasgn].include?(partial_assignment_ast.type)
+            partial_assignment_ast.append(full_rhs_ast)
+          end
         end
       end
     end
