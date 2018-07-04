248979a844ce54078ca2526db06f3d64ec45ffe8
Rafał Łasocha <me@swistak35.com>
2018-07-04 15:35:14 +0200

    Make GenericType and UnionType POROs

--- lib/orbacle/generic_type.rb
+++ lib/orbacle/generic_type.rb
@@ -1,5 +1,27 @@
 module Orbacle
-  class GenericType < Struct.new(:name, :parameters)
+  class GenericType
+    def initialize(name, parameters)
+      @name = name
+      @parameters = parameters
+    end
+
+    attr_reader :name, :parameters
+
+    def ==(other)
+      self.class == other.class &&
+        self.name == other.name &&
+        self.parameters == other.parameters
+    end
+
+    def hash
+      [
+        self.class,
+        self.name,
+        self.parameters,
+      ].hash ^ BIG_VALUE
+    end
+    alias eql? ==
+
     def each_possible_type
       yield self
     end
--- lib/orbacle/union_type.rb
+++ lib/orbacle/union_type.rb
@@ -1,5 +1,26 @@
 module Orbacle
-  class UnionType < Struct.new(:types)
+  class UnionType
+    def initialize(types)
+      @types = Set.new(types)
+    end
+
+    def types
+      @types.to_a
+    end
+
+    def ==(other)
+      self.class == other.class &&
+        self.types == other.types
+    end
+
+    def hash
+      [
+        self.class,
+        self.types,
+      ].hash ^ BIG_VALUE
+    end
+    alias eql? ==
+
     def each_possible_type
       types.each do |type|
         type.each_possible_type do |t|
