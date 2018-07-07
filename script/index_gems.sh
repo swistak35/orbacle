cdf9c299405bc9f0b294f189a5b6609ebb2c124a
Rafał Łasocha <me@swistak35.com>
2018-07-07 23:37:19 +0200

    Add folds

--- script/index_gems.sh
+++ script/index_gems.sh
@@ -16,21 +16,26 @@ make install
 
 for i in $(seq $start_gem_number $(expr $start_gem_number + $how_many))
 do
+  echo -en "travis_fold:start:${gem_full_name}\\r"
   echo "Processing $i"
   ./script/index_gem.sh $i $results_path
+  echo -en "travis_fold:end:${gem_full_name}\\r"
 done
 
 
 ### Upload results
+
+echo -en "travis_fold:start:upload\\r"
 
 cd tmp
 wget -q "https://github.com/dropbox/dbxcli/releases/download/v2.1.1/dbxcli-linux-amd64"
 chmod +x dbxcli-linux-amd64
 mkdir -p ~/.config/dbxcli/
 echo -n "{\"\":{\"personal\":\"$DROPBOX_KEY\"}}" > ~/.config/dbxcli/auth.json
-echo $(cat ~/.config/dbxcli/auth.json | sha1sum)
 
 for file in results/$current_sha/*
 do
   ./dbxcli-linux-amd64 put $file "orbacle/$file"
 done
+
+echo -en "travis_fold:end:upload\\r"
