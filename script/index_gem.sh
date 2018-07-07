41d66a80989350b2b56b7c5bd868089d45e20368
Rafał Łasocha <me@swistak35.com>
2018-07-07 20:43:16 +0200

    Index gems job

--- script/index_gem.sh
+++ script/index_gem.sh
@@ -4,14 +4,21 @@ set -e
 set -x
 
 gem_number=$1
+current_sha=$(git rev-parse HEAD)
+results_path="tmp/results/$current_sha"
 
 gem_info_json=$(cat script/most_popular_gems.json | head -n $gem_number | tail -n 1)
 gem_name=$(echo $gem_info_json | jq -r ".gem_name")
 gem_version=$(echo $gem_info_json | jq -r ".gem_version")
 gem_full_name=$(echo $gem_info_json | jq -r ".gem_full_name")
+error_log_path="$results_path/$gem_number.$gem_full_name.error.log"
+orbacle_log_path="$results_path/$gem_number.$gem_full_name.orbacle.log"
+stats_path="$results_path/$gem_number.$gem_full_name.stats.json"
+status_path="$results_path/$gem_number.$gem_full_name.status"
 
 gem_path="tmp/gems$gem_number"
 rm -rf $gem_path
 gem install --no-document --ignore-dependencies --install-dir $gem_path $gem_name -v $gem_version
-orbaclerun -d $gem_path/gems index 2> error.log >orbacle.log
-echo $? > status_code
+orbaclerun -d $gem_path/gems index 2> $error_log_path > $orbacle_log_path
+echo $? > $status_path
+mv stats.json $stats_path
---
+++ script/index_gems.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+set -x
+
+start_gem_number=$1
+how_many=$2
+
+rm -rf all_stats.json
+
+for i in $(seq $start_gem_number $(expr $start_gem_number + $how_many))
+do
+  echo "Processing $i"
+  ./script/index_gem.sh $i
+  cat stats.json >> all_stats.json
+  echo >> all_stats.json
+done
