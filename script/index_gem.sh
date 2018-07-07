51ee89e29dbe3287ca4cef4d34a758171a1003af
Rafał Łasocha <me@swistak35.com>
2018-07-07 21:20:25 +0200

    Final version of statistics

--- script/index_gem.sh
+++ script/index_gem.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
 
 gem_number=$1
-current_sha=$(git rev-parse HEAD)
-results_path="tmp/results/$current_sha"
+results_path=$2
 
-gem_info_json=$(cat script/most_popular_gems.json | head -n $(expr gem_number + 1) | tail -n 1)
+gem_info_json=$(cat script/most_popular_gems.json | head -n $(expr $gem_number + 1) | tail -n 1)
 gem_name=$(echo $gem_info_json | jq -r ".gem_name")
 gem_version=$(echo $gem_info_json | jq -r ".gem_version")
 gem_full_name=$(echo $gem_info_json | jq -r ".gem_full_name")
--- script/index_gems.sh
+++ script/index_gems.sh
@@ -5,10 +5,17 @@ set -x
 start_gem_number=$1
 how_many=$2
 
-rm -rf all_stats.json
+current_sha=$(git rev-parse HEAD)
+results_path="tmp/results/$current_sha"
+
+rm -rf $results_path
+mkdir -p $results_path
+
+make build
+make install
 
 for i in $(seq $start_gem_number $(expr $start_gem_number + $how_many))
 do
   echo "Processing $i"
-  ./script/index_gem.sh $i
+  ./script/index_gem.sh $i $results_path
 done
