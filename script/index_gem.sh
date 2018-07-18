b429ecec54db02f4d352fa71e1b474209d625024
Rafał Łasocha <me@swistak35.com>
2018-07-18 19:50:23 +0200

    Store only stats

--- script/index_gem.sh
+++ script/index_gem.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 gem_number=$1
 results_path=$2
@@ -7,10 +7,7 @@ gem_info_json=$(cat script/most_popular_gems.json | head -n $(expr $gem_number +
 gem_name=$(echo $gem_info_json | jq -r ".gem_name")
 gem_version=$(echo $gem_info_json | jq -r ".gem_version")
 gem_full_name=$(echo $gem_info_json | jq -r ".gem_full_name")
-error_log_path="$results_path/$gem_number.$gem_full_name.error.log"
-orbacle_log_path="$results_path/$gem_number.$gem_full_name.orbacle.log"
 stats_path="$results_path/$gem_number.$gem_full_name.stats.json"
-status_path="$results_path/$gem_number.$gem_full_name.status"
 
 echo -en "travis_fold:start:${gem_full_name}\\r"
 echo "Processing $gem_full_name"
@@ -18,8 +15,9 @@ echo "Processing $gem_full_name"
 gem_path="tmp/gems$gem_number"
 rm -rf $gem_path
 gem install --no-document --ignore-dependencies --install-dir $gem_path $gem_name -v $gem_version
-orbaclerun -d $gem_path/gems index 2> $error_log_path | tee $orbacle_log_path
-echo $? > $status_path
+
+orbaclerun -d $gem_path/gems index
+
 mv stats.json $stats_path
 
 echo -en "travis_fold:end:${gem_full_name}\\r"
