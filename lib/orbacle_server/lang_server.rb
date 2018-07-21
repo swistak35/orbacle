b748bee38462af0655deb3a81da60c0a00c1465e
Rafał Łasocha <me@swistak35.com>
2018-07-21 19:19:53 +0200

    Spike of the goto definition

--- lib/orbacle_server/lang_server.rb
+++ lib/orbacle_server/lang_server.rb
@@ -2,6 +2,56 @@ require 'lsp'
 require 'uri'
 
 module OrbacleServer
+  class FindDefinitionUnderPosition < Parser::AST::Processor
+    ConstantResult = Struct.new(:const_ref)
+
+    def initialize(parser)
+      @parser = parser
+    end
+
+    def process_file(file_content, searched_line, searched_character)
+      ast = parser.parse(file_content)
+
+      @current_nesting = Orbacle::Nesting.empty
+      @searched_position = Orbacle::Position.new(searched_line, searched_character)
+
+      process(ast)
+
+      @result
+    end
+
+    attr_reader :parser
+
+    def on_const(ast)
+      if build_position_range_from_ast(ast).include_position?(@searched_position.line, @searched_position.character)
+        @result = ConstantResult.new(Orbacle::ConstRef.from_ast(ast, @current_nesting))
+      end
+      return ast
+    end
+
+    def on_class(ast)
+      klass_name_ast, _ = ast.children
+      klass_name_ref = Orbacle::ConstRef.from_ast(klass_name_ast, @current_nesting)
+      with_new_nesting(@current_nesting.increase_nesting_const(klass_name_ref)) do
+        super
+      end
+      return ast
+    end
+
+    def build_position_range_from_ast(ast)
+      Orbacle::PositionRange.new(
+        Orbacle::Position.new(ast.loc.expression.begin.line - 1, ast.loc.expression.begin.column),
+        Orbacle::Position.new(ast.loc.expression.end.line - 1, ast.loc.expression.end.column - 1))
+    end
+
+    def with_new_nesting(new_nesting)
+      previous_nesting = @current_nesting
+      @current_nesting = new_nesting
+      yield
+      @current_nesting = previous_nesting
+    end
+  end
+
   class LangServer
     include Lsp::LanguageServer
 
@@ -31,6 +81,11 @@ module OrbacleServer
     end
 
     def handle_text_document_definition(request)
+      file_content = File.read(request.text_document.uri)
+      result = FindDefinitionUnderPosition.new.(file_content, request.position.line, request.position.character)
+      if result.is_a?(ConstantResult)
+      else
+      end
       Lsp::ResponseMessage.successful(
         Lsp::Location.new(
           "/home/swistak35/projs/msc/orbacle/lib/orbacle.rb",
---
+++ spec/find_definition_under_position_spec.rb
@@ -0,0 +1,62 @@
+require 'spec_helper'
+require 'orbacle'
+require 'orbacle_server'
+
+module OrbacleServer
+  RSpec.describe FindDefinitionUnderPosition do
+    specify do
+      file = <<-END
+      class Foo
+        def bar
+          Baz.new
+        end
+      end
+      END
+
+      expected_nesting = Orbacle::Nesting
+        .empty
+        .increase_nesting_const(Orbacle::ConstRef.from_full_name("Foo", Orbacle::Nesting.empty))
+      constant_result = FindDefinitionUnderPosition::ConstantResult.new(
+        Orbacle::ConstRef.from_full_name("Baz", expected_nesting))
+      expect(find_definition_under_position(file, 2, 9)).to be_nil
+      expect(find_definition_under_position(file, 2, 10)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 11)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 12)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 13)).to be_nil
+    end
+
+    specify do
+      file = <<-END
+      class Foo
+        def bar
+          ::Bar::Baz.new
+        end
+      end
+      END
+
+      expected_nesting = Orbacle::Nesting
+        .empty
+        .increase_nesting_const(Orbacle::ConstRef.from_full_name("Foo", Orbacle::Nesting.empty))
+      constant_result = FindDefinitionUnderPosition::ConstantResult.new(
+        Orbacle::ConstRef.from_full_name("::Bar::Baz", expected_nesting))
+      expect(find_definition_under_position(file, 2, 9)).to be_nil
+      expect(find_definition_under_position(file, 2, 10)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 11)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 12)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 13)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 14)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 15)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 16)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 17)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 18)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 19)).to eq(constant_result)
+      expect(find_definition_under_position(file, 2, 20)).to be_nil
+    end
+
+    def find_definition_under_position(file, line, column)
+      parser = Parser::CurrentRuby
+      service = OrbacleServer::FindDefinitionUnderPosition.new(parser)
+      service.process_file(file, line, column)
+    end
+  end
+end
